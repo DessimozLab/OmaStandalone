c24775ac695b24c7ba549d9a6aaf082c8f9f1ee4
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2016-05-11 19:05:13 +0200

    adjust script to work with both versions of cached allall files

--- utils/compact_allall.py
+++ utils/compact_allall.py
@@ -11,18 +11,20 @@ def compact_allall_folder(cachedir):
     :param cachedir: the path to the Cache folder to be compacted.
                      Don't add the AllAll folder."""
  
-    part_re = re.compile(r'(?P<species>\w*)_(?P<nr>\d+)-(?P<tot>\d+).gz')
-    allall_root = os.path.join(cachedir, 'AllAll')
+    allall_root = os.path.normpath(os.path.join(cachedir, 'AllAll'))
     _dirs_top = [ f for f in os.listdir(allall_root) 
         if os.path.isdir(os.path.join(allall_root,f)) ]
     buf_size = 2**16
     
     for _dir in _dirs_top:
-        allfiles = [f for f in os.listdir(os.path.join(allall_root, _dir ))
-                        if os.path.isfile(os.path.join(allall_root, _dir, f))]
+        allfiles = []
+        for d, subd, dfiles in os.walk(os.path.join(allall_root, _dir)): 
+            allfiles.extend(list(map(lambda f: os.path.join(d,f), dfiles)))
         tots = collections.defaultdict(int)
         cnts = collections.defaultdict(int)
         files = collections.defaultdict(list)    
+        part_re = re.compile(r'.*{0}{1}(?P<species>\w*)({1}part)?_(?P<nr>\d+)-(?P<tot>\d+).gz'.format(
+            os.path.join(allall_root, _dir), os.path.sep))
         for _file in allfiles:
             match = part_re.match(_file);
             if match:
@@ -34,14 +36,26 @@ def compact_allall_folder(cachedir):
     
         for spec2 in tots:
             if cnts[spec2]==tots[spec2]:
+                outfn = os.path.join(allall_root, _dir, spec2 + ".gz")
+                if os.path.isfile(outfn):
+                    print('{} already exists. Leaving part files untouched!'.format(outfn))
+                    continue
+
                 with open(os.path.join(allall_root, _dir, spec2 + ".gz"), 'wb') as dest:
                     for f in files[spec2]:
-                        with open(os.path.join(allall_root, _dir, f), 'rb') as src:
+                        with open(f, 'rb') as src:
                             for chunk in iter(lambda:src.read(buf_size), ''):
+                                if chunk==b'':
+                                    break
                                 dest.write(chunk)
                 for f in files[spec2]:
-                    os.remove(os.path.join(allall_root, _dir, f))
-                print('compacted '+spec2)
+                    os.remove(f)
+                try:
+                    os.rmdir(os.path.join(allall_root, _dir, spec2))
+                except FileNotFoundError:
+                    pass
+                  
+                print('compacted {} vs {}'.format(_dir, spec2))
 
 if __name__ == "__main__":
     import argparse
